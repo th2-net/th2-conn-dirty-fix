bdabdf2dd587cc74c6f7d33584c1a8f3375045cd
ivan.druzhinin <ivan.druzhinin@exactprosystems.com>
2022-01-21 16:06:23 +0300

    Initial mangler version

---
+++ src/main/kotlin/com/exactpro/th2/conn/dirty/fix/FixByteBufUtil.kt
@@ -0,0 +1,436 @@
+/*
+ * Copyright 2022-2022 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exactpro.th2.conn.dirty.fix
+
+import com.exactpro.th2.conn.dirty.tcp.core.util.EMPTY_STRING
+import com.exactpro.th2.conn.dirty.tcp.core.util.indexOf
+import com.exactpro.th2.conn.dirty.tcp.core.util.insert
+import com.exactpro.th2.conn.dirty.tcp.core.util.lastIndexOf
+import com.exactpro.th2.conn.dirty.tcp.core.util.replace
+import com.exactpro.th2.conn.dirty.tcp.core.util.requireReadable
+import com.exactpro.th2.conn.dirty.tcp.core.util.subsequence
+import io.netty.buffer.ByteBuf
+import java.nio.charset.Charset
+import kotlin.text.Charsets.UTF_8
+
+const val SOH_CHAR = '\u0001'
+const val SOH_BYTE = SOH_CHAR.code.toByte()
+const val SEP_CHAR = '='
+const val SEP_BYTE = '='.code.toByte()
+
+@JvmOverloads
+fun ByteBuf.firstField(charset: Charset = UTF_8): FixField? = FixField.atOffset(this, readerIndex(), charset)
+
+@JvmOverloads
+fun ByteBuf.lastField(charset: Charset = UTF_8): FixField? = FixField.atOffset(this, writerIndex() - 1, charset)
+
+@JvmOverloads
+inline fun ByteBuf.forEachField(charset: Charset = UTF_8, action: (FixField) -> Unit) {
+    var field = firstField(charset)
+
+    while (field != null) {
+        action(field)
+        field = field.next()
+    }
+}
+
+@JvmOverloads
+inline fun ByteBuf.forEachFieldDesc(charset: Charset = UTF_8, action: (FixField) -> Unit) {
+    var field = lastField(charset)
+
+    while (field != null) {
+        action(field)
+        field = field.previous()
+    }
+}
+
+@JvmOverloads
+inline fun ByteBuf.findField(charset: Charset = UTF_8, predicate: (FixField) -> Boolean): FixField? {
+    forEachField(charset) { if (predicate(it)) return it }
+    return null
+}
+
+@JvmOverloads
+inline fun ByteBuf.findLastField(charset: Charset = UTF_8, predicate: (FixField) -> Boolean): FixField? {
+    forEachFieldDesc(charset) { if (predicate(it)) return it }
+    return null
+}
+
+@JvmOverloads
+fun ByteBuf.findField(tag: Int?, charset: Charset = UTF_8): FixField? {
+    return findField(charset) { it.tag == tag }
+}
+
+@JvmOverloads
+fun ByteBuf.findField(tag: Int?, value: String?, charset: Charset = UTF_8): FixField? {
+    return findField(charset) { it.tag == tag && it.value == value }
+}
+
+@JvmOverloads
+fun ByteBuf.findLastField(tag: Int?, charset: Charset = UTF_8): FixField? {
+    return findLastField(charset) { it.tag == tag }
+}
+
+@JvmOverloads
+fun ByteBuf.findLastField(tag: Int?, value: String?, charset: Charset = UTF_8): FixField? {
+    return findLastField(charset) { it.tag == tag && it.value == value }
+}
+
+@JvmOverloads
+inline fun ByteBuf.modifyField(
+    predicate: (FixField) -> Boolean,
+    charset: Charset = UTF_8,
+    action: (FixField) -> Unit,
+): Boolean = findField(charset, predicate)?.apply(action) != null
+
+@JvmOverloads
+inline fun ByteBuf.modifyLastField(
+    predicate: (FixField) -> Boolean,
+    charset: Charset = UTF_8,
+    action: (FixField) -> Unit,
+): Boolean = findLastField(charset, predicate)?.apply(action) != null
+
+@JvmOverloads
+fun ByteBuf.setField(tag: Int?, value: String?, charset: Charset = UTF_8): Boolean {
+    return modifyField({ it.tag == tag }, charset) { it.value = value }
+}
+
+@JvmOverloads
+fun ByteBuf.setLastField(tag: Int?, value: String?, charset: Charset = UTF_8): Boolean {
+    return modifyLastField({ it.tag == tag }, charset) { it.value = value }
+}
+
+@JvmOverloads
+fun ByteBuf.addFieldAfter(
+    tag: Int?,
+    value: String?,
+    field: FixField = lastField() ?: error("message is empty"),
+): FixField = field.insertNext(tag, value)
+
+@JvmOverloads
+fun ByteBuf.addFieldBefore(
+    tag: Int?,
+    value: String?,
+    field: FixField = firstField() ?: error("message is empty"),
+): FixField = field.insertPrevious(tag, value)
+
+@JvmOverloads
+fun ByteBuf.replaceFieldValue(
+    tag: Int?,
+    oldValue: String?,
+    newValue: String?,
+    charset: Charset = UTF_8,
+): Boolean {
+    return modifyField({ it.tag == tag && it.value == oldValue }, charset) { it.value = newValue }
+}
+
+@JvmOverloads
+fun ByteBuf.replaceLastFieldValue(
+    tag: Int?,
+    oldValue: String?,
+    newValue: String?,
+    charset: Charset = UTF_8,
+): Boolean {
+    return modifyLastField({ it.tag == tag && it.value == oldValue }, charset) { it.value = newValue }
+}
+
+inline fun ByteBuf.getFields(
+    charset: Charset = UTF_8,
+    crossinline predicate: (FixField) -> Boolean,
+) = iterator {
+    forEachField(charset) { if (predicate(it)) yield(it) }
+}
+
+inline fun ByteBuf.getFieldsDesc(
+    charset: Charset = UTF_8,
+    crossinline predicate: (FixField) -> Boolean,
+) = iterator {
+    forEachFieldDesc(charset) { if (predicate(it)) yield(it) }
+}
+
+fun ByteBuf.calculateChecksum(): Int {
+    var checksum = 0
+
+    forEachField { field ->
+        if (field.tag == 10 && field.next() == null) return@forEachField
+        checksum += field.bytes.sum()
+    }
+
+    return checksum % 256
+}
+
+fun ByteBuf.setChecksum(checksum: Int): Boolean {
+    val value = "%03d".format(checksum)
+
+    if (!setField(10, value)) {
+        lastField()?.insertNext(10, value) ?: return false
+    }
+
+    return true
+}
+
+fun ByteBuf.updateChecksum(): Boolean = setChecksum(calculateChecksum())
+
+fun ByteBuf.calculateLength(): Int {
+    var length = 0
+    var field = findField(9) ?: return -1
+
+    while (true) {
+        field = field.next() ?: return length
+        if (field.tag == 10) return length
+        length += field.bytes.size
+    }
+}
+
+fun ByteBuf.setLength(length: Int): Boolean {
+    val value = length.toString()
+
+    if (!setField(9, value)) {
+        findField(8)?.insertNext(9, value) ?: return false
+    }
+
+    return true
+}
+
+fun ByteBuf.updateLength(): Boolean = when (val length = calculateLength()) {
+    -1 -> false
+    else -> setLength(length)
+}
+
+class FixField private constructor(
+    private val buffer: ByteBuf,
+    private var startIndex: Int,
+    private var endIndex: Int,
+    private val charset: Charset = UTF_8,
+) {
+    private var previous: FixField? = null
+    private var next: FixField? = null
+
+    private var _bytes: ByteArray? = null
+    private var _raw: String? = null
+    private var _rawTag: String? = null
+    private var _tag: Int? = null
+    private var _value: String? = null
+
+    var bytes: ByteArray
+        get() = _bytes ?: buffer.subsequence(startIndex, endIndex).apply {
+            _bytes = this
+        }
+        set(bytes) {
+            buffer.replace(startIndex, endIndex, bytes)
+            endIndex = startIndex + bytes.size
+            updateNextIndices()
+            _bytes = bytes
+            _raw = null
+            _rawTag = null
+            _tag = null
+            _value = null
+        }
+
+    var raw: String
+        get() = _raw ?: bytes.toString(charset).apply {
+            _raw = this
+        }
+        set(raw) {
+            bytes = raw.toByteArray(charset)
+            _raw = raw
+        }
+
+    var rawTag: String?
+        get() = _rawTag ?: raw.run {
+            val tag = substringBefore(SEP_CHAR)
+            if (tag == this) return null
+            _rawTag = tag
+            _tag = null
+            return _rawTag
+        }
+        set(rawTag) {
+            val value = value
+            raw = toRaw(rawTag, value)
+            _rawTag = rawTag
+            _tag = null
+            _value = value
+        }
+
+    var tag: Int?
+        get() = _tag ?: rawTag?.toIntOrNull().apply {
+            _tag = this
+        }
+        set(tag) {
+            rawTag = tag?.toString()
+            _tag = tag
+        }
+
+    var value: String?
+        get() = _value ?: raw.run {
+            val value = substringAfter(SEP_CHAR)
+            if (value == this) return null
+            _value = if (value.endsWith(SOH_CHAR)) value.substring(0, value.lastIndex) else value
+            return _value
+        }
+        set(value) {
+            val rawTag = rawTag
+            val tag = tag
+            raw = toRaw(rawTag, value)
+            _rawTag = rawTag
+            _tag = tag
+            _value = value
+        }
+
+    fun insertPrevious(bytes: ByteArray): FixField {
+        val previous = FixField(buffer, startIndex, startIndex + bytes.size)
+
+        buffer.insert(bytes, startIndex)
+
+        this.previous?.next = previous
+        previous.previous = this.previous
+        previous.next = this
+        this.previous = previous
+
+        previous.updateNextIndices()
+
+        return previous
+    }
+
+    fun insertPrevious(raw: String): FixField = insertPrevious(raw.toByteArray(charset))
+
+    fun insertPrevious(tag: String?, value: String?): FixField = insertPrevious(toRaw(tag, value))
+
+    fun insertPrevious(tag: Int?, value: String?): FixField = insertPrevious(toRaw(tag, value))
+
+    fun insertNext(bytes: ByteArray): FixField {
+        val next = FixField(buffer, endIndex, endIndex + bytes.size)
+
+        buffer.insert(bytes, endIndex)
+
+        this.next?.previous = next
+        next.previous = this
+        next.next = this.next
+        this.next = next
+
+        next.updateNextIndices()
+
+        return next
+    }
+
+    fun insertNext(raw: String): FixField = insertNext(raw.toByteArray(charset))
+
+    fun insertNext(tag: String?, value: String?): FixField = insertNext(toRaw(tag, value))
+
+    fun insertNext(tag: Int?, value: String?): FixField = insertNext(toRaw(tag, value))
+
+    fun clear() {
+        bytes = byteArrayOf()
+    }
+
+    private fun updateNextIndices(): Unit? = next?.let { next ->
+        val indexDiff = endIndex - next.startIndex
+        next.startIndex += indexDiff
+        next.endIndex += indexDiff
+        next.updateNextIndices()
+    }
+
+    private fun toRaw(tag: Int?, value: String?): String {
+        return toRaw(tag?.toString(), value)
+    }
+
+    private fun toRaw(tag: String?, value: String?): String {
+        return "${tag ?: EMPTY_STRING}$SEP_CHAR${value ?: EMPTY_STRING}${SOH_CHAR}"
+    }
+
+    fun next(): FixField? = next ?: when (endIndex) {
+        buffer.writerIndex() -> null
+        else -> atOffset(
+            buffer,
+            endIndex,
+            charset,
+            endIndex,
+            buffer.writerIndex()
+        )?.also {
+            next = it
+            it.previous = this
+        }
+    }
+
+    fun previous(): FixField? = previous ?: when (startIndex) {
+        buffer.readerIndex() -> null
+        else -> atOffset(
+            buffer,
+            startIndex - 1,
+            charset,
+            buffer.readerIndex(),
+            startIndex
+        )?.also {
+            previous = it
+            it.next = this
+        }
+    }
+
+    fun first(): FixField {
+        var current = this
+
+        while (true) {
+            current = current.previous() ?: return current
+        }
+    }
+
+    fun last(): FixField {
+        var current = this
+
+        while (true) {
+            current = current.next() ?: return current
+        }
+    }
+
+    override fun toString() = raw
+
+    companion object {
+        @JvmOverloads
+        fun atOffset(
+            buffer: ByteBuf,
+            offset: Int,
+            charset: Charset = UTF_8,
+            fromIndex: Int = buffer.readerIndex(),
+            toIndex: Int = buffer.writerIndex(),
+        ): FixField? {
+            buffer.requireReadable(fromIndex, toIndex)
+
+            if (fromIndex == toIndex) {
+                return null
+            }
+
+            require(offset in fromIndex until toIndex) {
+                "Offset is outside of the specified range: $fromIndex..$toIndex"
+            }
+
+            val startIndex = buffer.run {
+                val sohIndex = lastIndexOf(SOH_BYTE, fromIndex, offset)
+                if (sohIndex < 0) fromIndex else sohIndex + 1
+            }
+
+            val endIndex = buffer.run {
+                val sohIndex = indexOf(SOH_BYTE, offset, toIndex)
+                if (sohIndex < 0) toIndex else sohIndex + 1
+            }
+
+            return when {
+                startIndex != endIndex -> FixField(buffer, startIndex, endIndex, charset)
+                else -> null
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/exactpro/th2/conn/dirty/fix/MessageTransformer.kt
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2022-2022 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exactpro.th2.conn.dirty.fix
+
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonIgnore
+import io.netty.buffer.ByteBuf
+import mu.KotlinLogging
+import java.util.regex.Pattern
+import kotlin.text.Charsets.UTF_8
+
+object MessageTransformer {
+    private val logger = KotlinLogging.logger {}
+
+    fun transform(message: ByteBuf, transform: List<Transform>): List<Action> {
+        logger.debug { "Processing message: ${message.toString(UTF_8)}" }
+
+        val executed = mutableListOf<Action>()
+
+        for ((conditions, actions) in transform) {
+            if (!conditions.all { it.matches(message) }) {
+                continue
+            }
+
+            actions.forEach { action ->
+                action.set?.apply {
+                    if (message.setField(tag, value)) {
+                        executed += action
+                    }
+                }
+
+                action.add?.also { field ->
+                    action.before?.find(message)?.let { next ->
+                        next.insertPrevious(field.tag, field.value)
+                        executed += action
+                    }
+
+                    action.after?.find(message)?.let { previous ->
+                        previous.insertNext(field.tag, field.value)
+                        executed += action
+                    }
+                }
+
+                action.remove?.find(message)?.let { field ->
+                    field.clear()
+                    executed += action
+                }
+
+                action.replace?.find(message)?.let { field ->
+                    val with = action.with!!
+                    field.tag = with.tag
+                    field.value = with.value
+                    executed += action
+                }
+            }
+        }
+
+        if (executed.isEmpty()) {
+            logger.debug { "No transformation were applied" }
+            return executed
+        }
+
+        executed.forEach { logger.debug { "Applied transformation: $it" } }
+
+        if (transform.any(Transform::updateLength)) {
+            message.updateLength()
+            logger.debug { "Recalculated length" }
+        }
+
+        if (transform.any(Transform::updateChecksum)) {
+            message.updateChecksum()
+            logger.debug { "Recalculated checksum" }
+        }
+
+        return executed
+    }
+}
+
+data class FieldSelector(
+    val tag: Int,
+    val matches: Pattern,
+) {
+    @JsonIgnore private val predicate = matches.asMatchPredicate()
+
+    fun matches(message: ByteBuf): Boolean = find(message) != null
+
+    fun find(message: ByteBuf): FixField? = message.findField {
+        it.tag == this.tag && it.value?.run(this.predicate::test) ?: false
+    }
+
+    override fun toString() = buildString {
+        append("tag $tag")
+        append(" ~= /$matches/")
+    }
+}
+
+data class FieldDefinition(
+    val tag: Int,
+    val value: String,
+) {
+    override fun toString() = "tag $tag = '$value'"
+}
+
+data class Action(
+    val set: FieldDefinition? = null,
+    val add: FieldDefinition? = null,
+    val replace: FieldSelector? = null,
+    val remove: FieldSelector? = null,
+    val with: FieldDefinition? = null,
+    val before: FieldSelector? = null,
+    val after: FieldSelector? = null,
+) {
+    init {
+        val operations = listOfNotNull(set, add, remove, replace)
+
+        require(operations.isNotEmpty()) { "Action must have at least one operation" }
+        require(operations.size == 1) { "Action has more than one operation" }
+
+        require(set == null && remove == null || with == null && before == null && after == null) {
+            "'set'/'remove' operations cannot have 'with', 'before', 'after' options"
+        }
+
+        require(add == null || with == null) {
+            "'add' operations cannot have 'with' option"
+        }
+
+        require(replace == null || with != null) {
+            "'replace' operation requires 'with' option"
+        }
+
+        require(before == null || after == null) {
+            "'before' and 'after' options are mutually exclusive"
+        }
+
+        require(add == null || before != null || after != null) {
+            "'add' option requires 'before' or 'after' option"
+        }
+    }
+
+    override fun toString() = buildString {
+        set?.apply { append("set $this") }
+        add?.apply { append("add $this") }
+        replace?.apply { append("replace $this") }
+        remove?.apply { append("remove $this") }
+        with?.apply { append(" with $this") }
+        before?.apply { append(" before $this") }
+        after?.apply { append(" after $this") }
+    }
+}
+
+data class Transform(
+    @JsonAlias("when") val conditions: List<FieldSelector>,
+    @JsonAlias("then") val actions: List<Action>,
+    @JsonAlias("update-length") val updateLength: Boolean = true,
+    @JsonAlias("update-checksum") val updateChecksum: Boolean = true,
+) {
+    init {
+        require(conditions.isNotEmpty()) { "Transformation must have at least one condition" }
+        require(actions.isNotEmpty()) { "Transformation must have at least one action" }
+    }
+
+    override fun toString() = buildString {
+        appendLine("when")
+        conditions.forEach { appendLine("    $it") }
+        appendLine("then")
+        actions.forEach { appendLine("    $it") }
+    }
+}
